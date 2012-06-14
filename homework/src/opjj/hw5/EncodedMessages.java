80058e41f6e9ea558380bc68d134a672cc01b857
Miro Bezjak <bezjak.miro@gmail.com>
2012-06-15 00:15:57 +0200

    Messages are public static fields.

--- homework/src/opjj/hw5/EncodedMessages.java
+++ homework/src/opjj/hw5/EncodedMessages.java
@@ -2,7 +2,7 @@ package opjj.hw5;
 
 public final class EncodedMessages {
 
-    private static final String MESSAGE_1 = "...";
-    private static final String MESSAGE_2 = "...";
+    public static final String MESSAGE_1 = "...";
+    public static final String MESSAGE_2 = "...";
 
 }
