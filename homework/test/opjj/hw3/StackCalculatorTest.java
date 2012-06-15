31d19ebe58794c0081d35509a390a1c6f48177e5
Miro Bezjak <bezjak.miro@gmail.com>
2012-06-15 20:23:22 +0200

    Copy tests from code examples to homework assignments for easy bootstrap.

--- homework/test/opjj/hw3/StackCalculatorTest.java
+++ homework/test/opjj/hw3/StackCalculatorTest.java
@@ -1,5 +1,45 @@
 package opjj.hw3;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class StackCalculatorTest {
+
+    private StackCalculator calc = new StackCalculator();
+
+    @Test
+    public void pushShouldAddNumberToTheStack() {
+        int n = 42;
+        calc.push(n);
+
+        assertEquals(n, calc.result());
+        assertEquals(1, calc.size());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void addShouldThrowExceptionOnNoOperands() {
+        calc.add();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void addShouldThrowExceptionOnOnlyOneOperand() {
+        calc.push(42).add();
+    }
+
+    @Test
+    public void addShouldAddTwoOperandsAndPushResultOnTheStack() {
+        int a = 1;
+        int b = 5;
+        calc.push(a).push(b).add();
+
+        assertEquals(a + b, calc.result());
+        assertEquals(1, calc.size());
+    }
+
+    @Test
+    public void resultShouldReturn0OnEmptyStack() {
+        assertEquals(0, calc.result());
+    }
 
 }
